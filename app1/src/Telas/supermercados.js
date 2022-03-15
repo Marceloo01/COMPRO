3bb02c2c1374a7bd93e15d18391d826a8b8129ec
embrulhadinho <marcelinhooliveira2090@gmail.com>
2022-03-15 16:31:54 -0300

    chuva ;-;

---
+++ app1/src/Telas/supermercados.js
@@ -0,0 +1,22 @@
+import react from "react";
+import "../css/TelaSupermercados.css"
+
+export default function superMer() {
+
+    return (
+        <>
+        <div className="bodySuper">
+            <div className="super">
+                <h1>Veja os Supermercdos</h1>
+            </div>
+            <div className="super1">
+                <div className="Stxt">
+                    Primeiro Colocado
+                </div>
+                <div className="Simg"></div>
+            </div>
+        </div>
+
+        </>
+    )
+}
