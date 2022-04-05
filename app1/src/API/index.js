ead4cb106a746c0b8fc3e9c48ed86876d73bc996
embrulhadinho <93730832+embrulhadinho@users.noreply.github.com>
2022-04-05 13:36:49 -0300

    Delete app1/src/API directory

--- app1/src/API/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-require('./rotas/rotas')(app)
-const porta=1303;
-
-app.listen(porta,()=>{
-    console.log("Servidor back-end rodando")
-})
