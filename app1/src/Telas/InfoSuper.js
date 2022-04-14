86c202dcc7e613ba83c9739bfb0f46f617849266
embrulhadinho <marcelinhooliveira2090@gmail.com>
2022-04-14 17:25:03 -0300

    Arrumamos o rodapé e o slide supermercados, também adicionamos a nossa api no projeto react

--- app1/src/Telas/InfoSuper.js
+++ app1/src/Telas/InfoSuper.js
@@ -1,20 +1,36 @@
+import React, { useEffect , useState} from "react";
 import Supermercado from "../img/SupermercadoSlide.jpg";
 import "../css/infoSuper.css"
+import api from '../service/api_compro'
 
 
 export default function InfoSupermercados () {
+    const [supermercado,setSupermercado] = useState({});
+    
+    useEffect (() => {
+        var id= window.location.href.split('=')[1]
+        console.log(id)
+        api
+            .get(`/supermercado/exibir/${id}`)
+            .then((response) => {
+                setSupermercado(response.data)
+                console.log(supermercado);})
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+        },[])
     return (
         <>
         <div className="conjunto-info">
             <div className="Supermercado-img-info">
                 <img src={Supermercado}/>
-            </div><h1>Senhor dos pasteis</h1>
+            </div><h1>{supermercado.nome}</h1>
             <div className="supermercado-info">
                 
-                <p><span className="dado">Localização:</span> Avenida Baden Powell - Jardim Nova Europa, Campinas - SP</p>
+                <p><span className="dado">Localização:</span> {supermercado.rua} - {supermercado.bairro}, {supermercado.cidade} - {supermercado.estado}</p>
                 <p><span className="dado">Estado:</span> São Paulo</p>
                 <p><span className="dado">Cidade:</span> Campinas</p>
-                <p><span className="dado">e-mail:</span> precioso@gmail.com</p>
+                <p><span className="dado">e-mail:</span> {supermercado.email}</p>
                 <p><span className="dado">nota:</span> 7.1</p>
                 <p><span className="dado">ano:</span> 1900</p>
             </div>
