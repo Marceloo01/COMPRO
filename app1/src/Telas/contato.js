644188c0fe991e5d4c3dc863fbb964c88dda6116
embrulhadinho <marcelinhooliveira2090@gmail.com>
2022-04-20 17:12:13 -0300

    Criação da tela contato, teste no login e melhor organização na parte que apresenta os dados dos supermercados

--- app1/src/Telas/contato.js
+++ app1/src/Telas/contato.js
@@ -1,27 +1,23 @@
 import react from 'react';
 import '../css/contato.css';
+import Integrantes from '../componentes/integrante.js';
+import Yulian from '../img/integrantes/Yulian.jpg'
+import Marcelo from '../img/integrantes/Marcelinho.jpg'
+import Nat from '../img/integrantes/IMG_1788.jpg'
+import Thaissa from '../img/integrantes/Thaissa.jpg'
+import Renato from '../img/integrantes/Renato.jpg'
 
 export default function Contato (){
     return (
         <>
-        <div className="titulo">
-            <h1>Contato</h1>
-        </div>
-        <div className="subititulo">
-            formas de contato
-        </div>
-        <div className="formas-contato">
-            <strong>Responsaveis: </strong>Marcelo, Yulian, Natiely, Renato, Thaissa<br/>
-            <strong>E-mail: </strong>knoob077 /marcelinhooliveira2090<br/>
-            <strong>Whatsapp: </strong>12991747752<br/>
-        </div>
-
-        <div className="subititulo">
-            Sobre nós
-        </div>
-        <div className="subititulo">
-            
-        </div>
+            <div className="titulo"><h1>Contato</h1></div>
+            <div id="contato">
+                <Integrantes nome="Marcelo Augusto de Olivera" img={Marcelo} email="marcelinhooliveria2090" cargo="Programador Front-End"/>
+                <Integrantes nome="Yulian Santiago Almanza Almanza" img={Yulian} email="knoob0733" cargo="Programador Back-End"/>
+                <Integrantes nome="Natiely" img={Nat} email="natielyandrade30" cargo="Diretora"/>
+                <Integrantes nome="Thaissa" img={Thaissa} email="ThaissaSenai" cargo="Marketing"/>
+                <Integrantes nome="Renato" img={Renato} email="renatoribeiro.256991" cargo="Marketing"/>
+            </div>
         </>
     )
 }
