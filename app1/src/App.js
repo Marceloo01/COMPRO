d77e484fb7704a9f18f72a9ad3ae516a37954f3a
Marcelo <93730832+embrulhadinho@users.noreply.github.com>
2022-06-02 08:24:45 -0300

    Update App.js

--- app1/src/App.js
+++ app1/src/App.js
@@ -28,7 +28,7 @@ function App() {
                     "Procure por seus supermercados favoritos e mais próximos de você, veja alguns dados deles aqui",
                     "Adicione produtos ao seu carrinho para comparar preços em varios supermercados da sua cidade",
                     "Entre em contato com nossa equipe",
-                    "Procure tópicos que responderam suas duvidas sobre nosso site"];
+                    "Procure tópicos que responderão suas duvidas sobre nosso site"];
   // login
   const [sel,setSel]=useState(1);
   const [logou,setLogou]=useState(1); 
