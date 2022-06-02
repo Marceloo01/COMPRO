4fd2104d9c79ab8cd054b866deb4bb3d09b30006
Marcelo <93730832+embrulhadinho@users.noreply.github.com>
2022-06-02 08:09:04 -0300

    Update contato.js

--- app1/src/Telas/contato.js
+++ app1/src/Telas/contato.js
@@ -15,7 +15,7 @@ export default function Contato (props){
             <div id="contato">
                 <Integrantes nome="Marcelo Augusto" img={Marcelo} email="marcelinhooliveria2090" cargo="ProgramadorFront-End" numero="12 99174-7752"/>
                 <Integrantes nome="Yulian Santiago" img={Yulian} email="knoob0733" cargo="Programador Back-End" numero="12 99171-2418"/>
-                <Integrantes nome="Natiely Vitor" img={Nat} email="natielyandrade30" cargo="Diretora" numero="12 99168-2912"/>
+                <Integrantes nome="Natiely Vitor" img={Nat} email="natielyandrade30" cargo="Diretora" numero="12 99168-2913"/> 
                 <Integrantes nome="Thaissa Guedes" img={Thaissa} email="thaissaguedes16" cargo="Coordenadora" numero="12 99139-9087"/>
                 <Integrantes nome="Renato Ribeiro" img={Renato} email="renatoribeiro.256991" cargo="Coordenador" numero="12 99112-9009"/>
             </div>
